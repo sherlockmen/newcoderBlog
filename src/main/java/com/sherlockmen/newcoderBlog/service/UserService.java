32bfb0beb93bc0bd3746c94f787b2752c472e58c
sherlockmen <1539631420@qq.com>
2021-10-30 17:27:57 +0800

    blogv2 更新了注册页面，可以实现更换头像，修改密码。 添加了拦截器，可以将用户信息暂存在拦截器中，而不保存在session中

--- src/main/java/com/sherlockmen/newcoderBlog/service/UserService.java
+++ src/main/java/com/sherlockmen/newcoderBlog/service/UserService.java
@@ -159,4 +159,12 @@ public class UserService implements NewCoderBlogConstant{
     public void logout(String ticket){
         loginTicketMapper.updateStatus(ticket,1);
     }
+
+    public LoginTicket findLoginTicket(String ticket){
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
+    public int updateHeader(int userId, String headerUrl){
+        return userMapper.updateHeader(userId,headerUrl);
+    }
 }
