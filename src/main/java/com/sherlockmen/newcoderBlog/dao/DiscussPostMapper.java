539d61aed495db634c375aa6b4b4caf8c06f742e
sherlockmen <1539631420@qq.com>
2021-11-03 20:35:03 +0800

    更新了评论功能 更新了私信功能 更新了全局处理日志功能 更新了404/500错误页面

--- src/main/java/com/sherlockmen/newcoderBlog/dao/DiscussPostMapper.java
+++ src/main/java/com/sherlockmen/newcoderBlog/dao/DiscussPostMapper.java
@@ -16,4 +16,10 @@ public interface DiscussPostMapper {
     //如果只有一个参数，并且在<if>中使用，则必须添加别名
     int selectDiscussPostRows(@Param("userId") int userId);
 
+    int insertDiscussPost(DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
+
+    int updateCommentCount(int id, int commentCount);
+
 }
