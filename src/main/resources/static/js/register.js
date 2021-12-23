6081c8e4cfc5cf00bc1e83fa3958be5b8391f55e
sherlockmen <1539631420@qq.com>
2021-12-23 21:02:27 +0800

    更新权限管理，加精、置顶、删除功能，以及新活跃用户UV和日活用户DAU汇总

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -4,6 +4,14 @@ $(function(){
 });
 
 function check_data() {
+
+	//发送AJAX请求之前，将CSRF令牌设置到请求的消息头中
+	var token = $("meta[name='_csrf']").attr("content");
+	var header = $("meta[name='_csrf_header']").attr("content");
+	$(document).ajaxSend(function (e, xhr, options) {
+		xhr.setRequestHeader(header,token)
+	});
+
 	var pwd1 = $("#password").val();
 	var pwd2 = $("#confirm-password").val();
 	if(pwd1 != pwd2) {
